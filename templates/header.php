253b5e5f2ce951e3f84d74c7c9ac8c6113feeb8c
phychole <145274220+phychole@users.noreply.github.com>
2025-09-25 08:05:47 +0700

    Add templates/header.php for HTML layout

---
+++ templates/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Sistem Prestasi Siswa</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+<body>
+<div class="container py-4">
+    <nav class="mb-4">
+        <a href="siswa.php" class="btn btn-secondary">Siswa</a>
+        <a href="prestasi.php" class="btn btn-secondary">Prestasi</a>
+        <a href="laporan.php" class="btn btn-secondary">Laporan</a>
+    </nav>
